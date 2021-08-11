360c69376dd52181a784a4f25889a2881f60d9d9
Isaac Balintuma <ibalintuma34@gmail.com>
2021-08-11 05:43:49 +0300

    Loan Application Meeting setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,15 +10,15 @@
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-    <body style="background-color: #c5c5c5 !important">
+    <body style="background-image: url('{{url("images/bg_2.jpg")}}');">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
                     <div class="container">
-                        <div class="row justify-content-center">
+                        <div class="row justify-content-center ">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-header"><h2 class="text-center font-weight-light my-4">Secular Loans</h2></div>
                                     <div class="card-body">
                                         <form method="POST" action="/authenticate">
                                             {{ csrf_field() }}
@@ -30,18 +30,13 @@
                                                 <input class="form-control" id="password" type="password" name="password" placeholder="Password" />
                                                 <label for="password">Password</label>
                                             </div>
-                                            <div class="form-check mb-3">
-                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small" href="/forgot_password">Forgot Password?</a>
-                                                <button class="btn btn-primary">Login</button>
+                                            <div class="text-center  mb-0">
+                                                <button class="btn btn-primary m-2" style=" width: 200px">Login</button>
                                             </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="/register">Need an account? Sign up!</a></div>
+                                        <div class="small mt-2"><p >This system is intended for administrators usage only</p></div>
                                     </div>
                                 </div>
                             </div>
